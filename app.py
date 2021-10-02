d50e15a5022b2a992b6b9c5a25c969cedd8d1274
Sasitha Gamage <sasithaeginfo@gmail.com>
2021-10-02 18:55:26 +0530

    updated application

--- app.py
+++ app.py
@@ -211,6 +211,5 @@ def dataImport():
         else:
             return render_template("segmentationcsvOutput.html",data = "Unsupported File Type")
 
-port = int(os.environ.get('PORT', 5000))
 
-app.run(host='0.0.0.0', port=port, debug=True)
+app.run(debug=True)
